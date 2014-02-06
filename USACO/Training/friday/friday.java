efa24d229062149beee3fc629487a2642d1ebf2e
177270 <177270@HSS-MC2-05.westwindsor.k12.nj.us>
2014-02-06 16:02:36 -0500

    usaco hard.

--- USACO/Training/friday/friday.java
+++ USACO/Training/friday/friday.java
@@ -1,5 +1,11 @@
 package friday;
 
+/*
+ ID: aleck.z1
+ LANG: JAVA
+ TASK: friday
+ */
+
 import java.io.*;
 
 public class friday
@@ -8,42 +14,59 @@ public class friday
 
 	public static void main(String[] args) throws IOException
 	{
-		BufferedReader br=new BufferedReader(new FileReader("friday.in"));
-		PrintWriter out=new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
-		
-		int numberYears=Integer.parseInt(br.readLine());
-		
-		int[][] regularYear = new int[12][31];
-		int[][] leapYear = new int[12][31];
+		BufferedReader br = new BufferedReader(new FileReader("friday.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"friday.out")));
+
+		int numberYears = Integer.parseInt(br.readLine());
 
 		int[] daysOfRegYear = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		int[] daysOfLeapYear = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-		for (int month = 0; month < 12; month++)
-		{
-			for (int day = 0; day < daysOfRegYear[month]; day++)
-				regularYear[month][day] = day + 1;
-		}
-		
-		for (int month = 0; month < 12; month++)
-		{
-			for (int day = 0; day < daysOfLeapYear[month]; day++)
-				leapYear[month][day] = day + 1;
-		}
-		
-		int dayOfWeekOfFirst=1;
-		
-		for(int year=0;year<numberYears;year++)
-		{
-			
-		}
-		
-		
-	}
+		int dayOfWeek1 = 2;
 
-//	private void increments(int year)
-//	{
-//		if(year%100!=0 && year%4==0)
-//			
-//	}
+		for (int year = 1900; year < 1900 + numberYears; year++)
+		{
+			if (year % 4 != 0)
+			{
+				for (int i = 0; i < daysOfRegYear.length; i++)
+				{
+					int dayOfWeek13 = (dayOfWeek1 + 12) % 7;
+					frequencies[dayOfWeek13]++;
+					dayOfWeek1 = (dayOfWeek1 + daysOfRegYear[i]) % 7;
+				}
+			}
+			else
+			{
+				if (year % 400 == 0)
+				{
+					for (int i = 0; i < daysOfLeapYear.length; i++)
+					{
+						int dayOfWeek13 = (dayOfWeek1 + 12) % 7;
+						frequencies[dayOfWeek13]++;
+						dayOfWeek1 = (dayOfWeek1 + daysOfLeapYear[i]) % 7;
+					}
+				}
+				else
+				{
+					for (int i = 0; i < daysOfRegYear.length; i++)
+					{
+						int dayOfWeek13 = (dayOfWeek1 + 12) % 7;
+						frequencies[dayOfWeek13]++;
+						dayOfWeek1 = (dayOfWeek1 + daysOfRegYear[i]) % 7;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < frequencies.length; i++)
+			System.out.println(frequencies[i]);
+
+		for (int i = 0; i < frequencies.length - 1; i++)
+			out.print(frequencies[i] + " ");
+		out.print(frequencies[6]+"\n");
+
+		out.close();
+		System.exit(0);
+	}
 }
